902d367d844a941abffb13b8e5da4da5af6f477a
Vinicius096 <vinicius_brennerr@hotmail.com>
2020-09-13 17:22:32 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 3

--- lists/tests.py
+++ lists/tests.py
@@ -55,5 +55,9 @@ class ItemModelTest(TestCase):
 
 class ListViewTest(TestCase):
 
+	def test_uses_list_template(self):
+		response = self.client.get('/lists/the-only-list-in-the-world/')
+		self.assertTemplateUsed(response, 'list.html')
+
 	def test_displays_all_items(self):
 		pass
