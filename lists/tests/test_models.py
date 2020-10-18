c7376b7325f1a876d1619841908a2b2b172ea36d
Vinicius Brenner <vinicius_brennerr@hotmail.com>
2020-10-18 13:22:44 -0300

    Validação: Prevenindo itens vazios - PARTE 2

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,33 +1,42 @@
 from django.test import TestCase
 from lists.models import Item, List
+from django.core.exceptions import ValidationError
+
 
 class ListAndItemModelsTest(TestCase):
 
-	def test_saving_and_retrieving_items(self):
-	
-		my_list = List()
-		my_list.save()
+    def test_saving_and_retrieving_items(self):
 
-		first_item = Item()
-		first_item.text = 'O primeiro item'
-		first_item.list = my_list
-		first_item.save()
+        my_list = List()
+        my_list.save()
 
-		second_item = Item()
-		second_item.text = 'O segundo item'
-		second_item.list = my_list
-		second_item.save()
+        first_item = Item()
+        first_item.text = 'O primeiro item'
+        first_item.list = my_list
+        first_item.save()
 
-		saved_list = List.objects.first()
-		self.assertEqual(saved_list, my_list)
+        second_item = Item()
+        second_item.text = 'O segundo item'
+        second_item.list = my_list
+        second_item.save()
 
-		saved_items = Item.objects.all()
-		self.assertEqual(saved_items.count(), 2)
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, my_list)
 
-		first_saved_item = saved_items[0]
-		second_saved_item = saved_items[1]
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
 
-		self.assertEqual(first_saved_item.text, 'O primeiro item')
-		self.assertEqual(first_saved_item.list, my_list)
-		self.assertEqual(second_saved_item.text, 'O segundo item')
-		self.assertEqual(second_saved_item.list, my_list)
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, 'O primeiro item')
+        self.assertEqual(first_saved_item.list, my_list)
+        self.assertEqual(second_saved_item.text, 'O segundo item')
+        self.assertEqual(second_saved_item.list, my_list)
+
+    def test_cannot_save_empty_list_items(self):
+    list_ = List.objects.create()
+    item = Item(list=list_, text='')
+    with self.assertRaises(ValidationError):
+        item.save()
+        item.full_clean()
